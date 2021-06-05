2000cb9eca999f90c8cd75a16aa7c38369eb4fd8
Djair Soares <djhanlj@gmail.com>
2021-06-05 12:21:53 -0300

    tela de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
+import SignUn from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <SignUn />
       <GlobalStyle />
     </>
   );
